82c6a542a9914b265e3397f50a9a181c684873d2
Sergey Boryaev <somsom.som2@yandex.ru>
2020-01-29 20:49:32 +0300

    Unworked

--- kur.cpp
+++ kur.cpp
@@ -16,7 +16,7 @@ public:
     }
     void PrintNumber()
     {
-        cout << "Your number is: " << number;
+        cout << "Your number is: " << number << endl;
     }
     long long GetLen()
     {
@@ -28,13 +28,34 @@ public:
     }
     void AddNumber(BigInt c)
     {
-        string secondNumber = c.GetNumber;
+        string secondNumber = c.GetNumber();
         long long lenDifference = numberLen - c.GetLen();
         if (lenDifference > 0)
-            secondNumber.insert(0,lenDifference,'0');
+            secondNumber.insert(0, lenDifference, '0');
         else
-            number.insert(0,lenDifference,'0');
-        
-        
+            number.insert(0, lenDifference, '0');
+        short fission = 0;
+        for (int i = numberLen; i >= 0; i--)
+        {
+            number[i] = ((number[i] + secondNumber[i]) - '0' * 2) % 10;
+            fission = ((number[i] + secondNumber[i]) - '0' * 2) / 10;
+            if (i)
+                number[i - 1] += fission;
+        }
+        if (fission)
+            number.insert(0, 1, '1');
     }
+};
+
+int main()
+{
+    string str1, str2;
+    cin >> str1 >> str2;
+    BigInt firstNumber, secondNumber;
+    firstNumber.EnterNumber(str1);
+    secondNumber.EnterNumber(str2);
+    firstNumber.PrintNumber();
+    secondNumber.PrintNumber();
+    firstNumber.AddNumber(secondNumber);
+    firstNumber.PrintNumber();
 }
